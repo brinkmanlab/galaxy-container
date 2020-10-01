566bc1aa9e0c0bf3f712b9c7436669ea673e15ab
Nolan Woods <nolan_w@sfu.ca>
2020-10-01 10:39:23 -0700

    Remove workflow scheduler hack and add destination specific config options

--- destinations/k8s/variables.tf
+++ destinations/k8s/variables.tf
@@ -1,6 +1,10 @@
 locals {
   name_suffix = var.instance == "" ? "" : "-${var.instance}"
   instance    = var.instance == "" ? "default" : var.instance
+  destination_galaxy_conf = {
+    #TODO AWS SQS amqp_internal_connection: https://docs.celeryproject.org/projects/kombu/en/stable/userguide/connections.html#urls
+    # https://docs.celeryproject.org/projects/kombu/en/latest/reference/kombu.transport.SQS.html#id1
+  }
 }
 
 variable "nfs_server" {
--- destinations/variables.tf
+++ destinations/variables.tf
@@ -37,10 +37,7 @@ locals {
   admin_users_conf = length(var.admin_users) == 0 ? {} : {
     admin_users = join(",", var.admin_users)
   }
-  temp_workflow_scheduler_hack = {
-    workflow_schedulers_config_file = "${local.config_dir}/macros/workflow_schedulers.xml"
-  }
-  galaxy_conf = merge(var.galaxy_conf, local.master_api_key_conf, local.admin_users_conf, local.temp_workflow_scheduler_hack)
+  galaxy_conf = merge(var.galaxy_conf, local.master_api_key_conf, local.admin_users_conf, local.destination_galaxy_conf)
 }
 
 variable "db_conf" {
@@ -236,5 +233,5 @@ variable "lb_annotations" {
 variable "tool_mappings" {
   type = map(string)
   default = {}
-  description = "Tool ID to destination mappings. Currently only default and big destinations are available."
+  description = "Tool ID to destination mappings. See roles/galaxy_app/defaults/main/job_conf.yml within the module root for destinations."
 }
