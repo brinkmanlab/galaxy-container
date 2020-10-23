cea65bded62251c85027d341d6c364accbd995e0
Nolan Woods <nolan_w@sfu.ca>
2020-10-23 07:50:19 -0700

    Add metadata fallback conf

--- destinations/k8s/variables.tf
+++ destinations/k8s/variables.tf
@@ -2,6 +2,7 @@ locals {
   name_suffix = var.instance == "" ? "" : "-${var.instance}"
   instance    = var.instance == "" ? "default" : var.instance
   destination_galaxy_conf = {
+    retry_metadata_internally = true  # k8s depends on the metadata fallback
   }
 }
 
