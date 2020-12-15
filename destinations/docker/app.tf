947f099d1fbff08441d19f3803c7ad8938329f50
Nolan Woods <nolan_w@sfu.ca>
2020-12-15 11:05:23 -0800

    avoid name collision for wait steps

--- destinations/docker/app.tf
+++ destinations/docker/app.tf
@@ -64,8 +64,9 @@ resource "docker_container" "galaxy_app" {
 }
 
 resource "docker_container" "wait_for_app" {
+  depends_on = [docker_container.galaxy_app]
   image = docker_image.galaxy_app.latest
-  name = "wait_for_app${local.name_suffix}"
+  name = "wait_for_galaxy_app${local.name_suffix}"
   must_run = false
   attach = true
   command = ["bash", "-c", "until uwping uwsgi://${local.app_name}:${local.uwsgi_port}/api/version; do sleep 1; done"]
