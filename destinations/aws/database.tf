c1e815a3b8c0a5c0081bc08b03cf98079c3ea3d4
Nolan Woods <nolan_w@sfu.ca>
2021-05-06 11:41:38 -0700

    Increase database ram

--- destinations/aws/database.tf
+++ destinations/aws/database.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "galaxy_db" {
   max_allocated_storage = 100
   storage_type          = "gp2"
   engine                = "postgres"    # https://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_CreateDBInstance.html
-  instance_class        = "db.t3.micro" # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
+  instance_class        = "db.t3.large" # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
   name                  = local.db_conf.name
   username              = local.db_conf.user
   password              = local.db_conf.pass
