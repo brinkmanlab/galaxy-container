a0f88eb920f14e124feac53891c9245f5a9abaa1
Nolan Woods <nolan_w@sfu.ca>
2022-08-31 19:09:54 -0700

    Set AWS RDS backup retention

--- destinations/aws/database.tf
+++ destinations/aws/database.tf
@@ -1,21 +1,22 @@
 resource "time_static" "now" {}
 
 resource "aws_db_instance" "galaxy_db" {
-  identifier            = "${local.db_name}${local.name_suffix}"
-  allocated_storage     = 20
-  max_allocated_storage = 100
-  storage_type          = "gp2"
-  engine                = "postgres"    # https://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_CreateDBInstance.html
-  instance_class        = "db.t3.large" # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
-  name                  = local.db_conf.name
-  username              = local.db_conf.user
-  password              = local.db_conf.pass
+  identifier                = "${local.db_name}${local.name_suffix}"
+  allocated_storage         = 20
+  max_allocated_storage     = 100
+  storage_type              = "gp2"
+  engine                    = "postgres"    # https://docs.aws.amazon.com/AmazonRDS/latest/APIReference/API_CreateDBInstance.html
+  instance_class            = "db.t3.large" # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
+  name                      = local.db_conf.name
+  username                  = local.db_conf.user
+  password                  = local.db_conf.pass
   #parameter_group_name = "default.mysql5.7"
   vpc_security_group_ids    = [var.eks.worker_security_group_id]
   db_subnet_group_name      = var.vpc.database_subnet_group
   publicly_accessible       = false
   skip_final_snapshot       = var.debug
   final_snapshot_identifier = "${local.db_conf.name}-${formatdate("YYYYMMDDhhmmss", time_static.now.rfc3339)}"
+  backup_retention_period   = 30
 }
 
 ## Register database in internal DNS
